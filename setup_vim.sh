a1eb3e7c90ef190ebabe1303ced6849f1371ea03
Nick Rushton <nick@hasoffers.com>
2016-04-06 13:09:36 -0700

    add setup for ctags

--- setup_vim.sh
+++ setup_vim.sh
@@ -14,3 +14,10 @@ git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/vim-colors
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 git clone https://github.com/tmhedberg/matchit.git ~/.vim/bundle/matchit
+
+# install ctags
+wget "http://downloads.sourceforge.net/project/ctags/ctags/5.8/ctags-5.8.tar.gz?r=http%3A%2F%2Fctags.sourceforge.net%2F&ts=1459972966&use_mirror=tenet" -O /tmp/ctags-5.8.tar.gz
+tar xzf /tmp/ctags-5.8.tar.gz -C /tmp/
+cd /tmp/ctags-5.8
+./configure && make && make install
+cd -
